3aab79ffcd8756f04f7d81ea4a32ed50703c97d4
Joao Felipe Rangel <joaofelipe.rfrade@gmail.com>
2025-04-28 12:48:10 -0300

    realizando testes

---
+++ smartTv/src/usuario.java
@@ -0,0 +1,13 @@
+public class usuario {
+    public static void main(String[] args) throws Exception {
+        smartTv smartTv = new smartTv();
+
+        System.out.println("Tv ligada? " + smartTv.ligada);
+        System.out.println("Qual canal? " + smartTv.canal);
+        System.out.println("Qual o volume? "+ smartTv.volume);
+
+
+
+
+    }
+}
