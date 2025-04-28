959acaf56721a4dff560116a428e66df1e5cb822
Joao Felipe Rangel <joaofelipe.rfrade@gmail.com>
2025-04-28 12:43:00 -0300

    criando os métodos de ligar/ desligar tv

---
+++ smartTv/src/smartTv.java
@@ -0,0 +1,16 @@
+public class smartTv {
+    boolean tvLigada= false;
+    int canal= 1;
+    int volume= 25;
+
+    //Ligar/ desligar TV
+    public void ligar (){
+        tvLigada= true;
+    }
+
+    public void desligar(){
+        tvLigada= false;
+    }
+
+
+}
