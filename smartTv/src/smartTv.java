6c92c2bcc2f57b723f241e88c84045dbed6ef5ce
Joao Felipe Rangel <joaofelipe.rfrade@gmail.com>
2025-04-28 12:55:46 -0300

    criando a  função ligar/desligar

--- smartTv/src/smartTv.java
+++ smartTv/src/smartTv.java
@@ -3,4 +3,11 @@ public class smartTv {
     int canal= 1;
     int volume= 25;
 
+    public void ligar(){
+        ligada= true;
+    }
+    public void desligada(){
+        ligada= false;
+    }
+
 }
